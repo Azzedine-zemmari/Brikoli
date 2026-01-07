797e1d999ad50dfc67dd0796689c28671f1987da
Azzedine-zemmari <azzedinezemmari@gmail.com>
2026-01-07 19:30:11 +0100

    cree l entity professional Profile avec skill aussi

---
+++ src/main/java/com/azzedine/brikoli/entity/ProfessionalProfile.java
@@ -0,0 +1,42 @@
+package com.azzedine.brikoli.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProfessionalProfile {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    private String bio;
+
+   @ManyToMany
+   @JoinTable(
+           name="professional_skills",
+           joinColumns = @JoinColumn(name = "professional_id"),
+           inverseJoinColumns = @JoinColumn(name="skills_id")
+   )
+    private List<Skill> Skills;
+
+   private String Location;
+
+   private Boolean isGraduated;
+
+   private Double ratingAverage;
+
+   private Integer completedMission;
+
+   @OneToOne
+   @JoinColumn(name="user_id")
+   private User user;
+}
