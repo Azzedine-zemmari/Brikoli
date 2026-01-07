7d9823e35b9c6284ba192af8122af6bd13934ea7
Azzedine-zemmari <azzedinezemmari@gmail.com>
2026-01-07 19:34:23 +0100

    cree l entity client profile

---
+++ src/main/java/com/azzedine/brikoli/entity/ClientProfile.java
@@ -0,0 +1,26 @@
+package com.azzedine.brikoli.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ClientProfile {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    private String address;
+
+    private String city;
+
+    @OneToOne
+    @JoinColumn(name="user_id")
+    private User user;
+}
