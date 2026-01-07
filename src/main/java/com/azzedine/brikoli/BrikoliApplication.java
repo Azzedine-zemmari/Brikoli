ce3b4fe4e4252e4f01d5baf70875c32bff41bc55
Azzedine-zemmari <azzedinezemmari@gmail.com>
2026-01-07 11:43:33 +0100

    start

---
+++ src/main/java/com/azzedine/brikoli/BrikoliApplication.java
@@ -0,0 +1,13 @@
+package com.azzedine.brikoli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrikoliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrikoliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/azzedine/brikoli/BrikoliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azzedine.brikoli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrikoliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
