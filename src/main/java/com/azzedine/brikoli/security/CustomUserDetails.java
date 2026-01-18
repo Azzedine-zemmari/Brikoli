d132efbcd262f22ce3defa7c54acdf77c181af4a
Azzedine-zemmari <azzedinezemmari@gmail.com>
2026-01-18 18:37:08 +0100

    ajouter user repository avec method find by email

---
+++ src/main/java/com/azzedine/brikoli/security/CustomUserDetails.java
@@ -0,0 +1,9 @@
+package com.azzedine.brikoli.security;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@Service
+public class CustomUserDetails implements UserDetailsService{
+    
+    
+}
