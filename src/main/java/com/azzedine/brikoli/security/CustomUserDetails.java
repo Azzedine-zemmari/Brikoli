64ec127b23b9e5dc69e27df84a9ae76d2d1bb68f
Azzedine-zemmari <azzedinezemmari@gmail.com>
2026-01-18 18:47:01 +0100

    ajouter custom user details

--- src/main/java/com/azzedine/brikoli/security/CustomUserDetails.java
+++ src/main/java/com/azzedine/brikoli/security/CustomUserDetails.java
@@ -1,9 +1,39 @@
 package com.azzedine.brikoli.security;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.GrantedAuthoritiesContainer;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.azzedine.brikoli.repository.UserRepository;
+import com.azzedine.brikoli.entity.User;
+
+import lombok.AllArgsConstructor;
+import java.util.Collections;
 
 @Service
-public class CustomUserDetails implements UserDetailsService{
-    
-    
+@AllArgsConstructor
+public class CustomUserDetails implements UserDetailsService {
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+        User user = userRepository.findByEmail(username).orElseThrow(()-> new UsernameNotFoundException(username));
+        
+        GrantedAuthority authority = new SimpleGrantedAuthority(String.valueOf(user.getRole()));
+
+        return org.springframework.security.core.userdetails.User
+            .withUsername(user.getEmail())
+            .password(user.getPassword())
+            .authorities(Collections.singletonList(authority))
+            .accountExpired(false)
+            .accountLocked(false)
+            .credentialsExpired(false)
+            .disabled(false)
+            .build();
+    }
+
 }
